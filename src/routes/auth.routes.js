f3626371a0ac648900ef426161a5f978311032d7
itsarbin <arbinshrestha63@gmail.com>
2026-02-18 18:11:21 +0530

    Initial commit - Auth API with register route

---
+++ src/routes/auth.routes.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const userModel = require('../model/user.model')
+const jwt = require('jsonwebtoken')
+
+const authRouter = express.Router();
+
+authRouter.post('/register',async (req,res)=>{
+    const {name,email,password} = req.body;
+
+    const existingUser = await userModel.findOne({email})
+
+    if(existingUser){
+        return res.status(401).json({
+            message:'Email already exists'
+        })
+    }
+
+
+
+    const user = await userModel.create({
+        name,
+        email,
+        password
+    })
+
+    const token = jwt.sign({id:user._id},process.env.JWT_SECRET)
+
+    res.cookie('token',token)
+
+    res.status(201).json({
+        message:'User registered successfully',
+        user,
+        token
+    })
+})
+    
+
+    
+
+
+
+
+
+
+
+module.exports = authRouter;
