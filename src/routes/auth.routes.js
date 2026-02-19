f5fe5729914fa7076410e995653b306603ab935e
itsarbin <arbinshrestha63@gmail.com>
2026-02-19 18:45:52 +0530

    login api added

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const userModel = require('../model/user.model')
 const jwt = require('jsonwebtoken')
+const crypto = require('crypto')
 
 const authRouter = express.Router();
 
@@ -15,12 +16,14 @@ authRouter.post('/register',async (req,res)=>{
         })
     }
 
+    const hashedPassword = crypto.createHash('md5').update(password).digest('hex')
+
 
 
     const user = await userModel.create({
         name,
         email,
-        password
+        password: hashedPassword
     })
 
     const token = jwt.sign({id:user._id},process.env.JWT_SECRET)
@@ -29,6 +32,35 @@ authRouter.post('/register',async (req,res)=>{
 
     res.status(201).json({
         message:'User registered successfully',
+        user,
+        token
+    })
+})
+
+authRouter.post('/login', async (req,res)=>{
+    const {email,password} = req.body;
+
+    const user = await userModel.findOne({email});
+
+    if(!user){
+        return res.status(401).json({
+            message:'Invalid email'
+        })
+    }
+
+    const isPasswordValid = user.password === crypto.createHash('md5').update(password).digest('hex');
+
+    if(!isPasswordValid){
+        return res.status(401).json({
+            message:'Invalid password'
+        })
+    }
+
+    const token = jwt.sign({id:user._id},process.env.JWT_SECRET);
+    res.cookie('token',token)
+
+    res.status(200).json({
+        message:'Login successful',
         user,
         token
     })
